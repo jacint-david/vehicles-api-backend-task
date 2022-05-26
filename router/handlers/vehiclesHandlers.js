2269e9bd85eda4cfc8de5ce52c917cd448abee30
Jacint M David <jacint.m.david@gmail.com>
2022-05-26 15:58:49 +0100

    Added makeModel field to response, omitted timestamps, added delete endpoint

--- router/handlers/vehiclesHandlers.js
+++ router/handlers/vehiclesHandlers.js
@@ -3,12 +3,33 @@ const db = require('../../database/models/index');
 module.exports = {
   getAllVehicles: async (req, res) => {
     try {
-      const vehicles = await db.Vehicle.findAll();
-      res.json(vehicles);
+      const vehicles = await db.Vehicle.findAll({raw: true});
+      // vehicles.forEach(vehicle => {vehicle["makeModel"] = `${vehicle.make} - ${vehicle.model}`});
+
+      const vehiclesWithoutTimestamps = vehicles.map(vehicle => {
+        return ({
+        id : vehicle.id,
+        make: vehicle.make,
+        model: vehicle.model,
+        makeModel: `${vehicle.make} - ${vehicle.model}`
+      })
+    })
+      console.log(vehiclesWithoutTimestamps);
+      res.json(vehiclesWithoutTimestamps);
+
     } catch (err) {
       console.log("Couldn't get all vehicles due to this error:", err);
     }
   },
+  deleteVehicleById : async (req, res) => {
+    const id = req.params.id;
+    const vehicleToDelete = await db.Vehicle.destroy({where: {
+      id
+    }});
+    console.log(vehicleToDelete);
+    res.sendStatus(204);
+  },
+
   getInstructions: async (req, res) => {
     res.send("Use the '/api/v1/vehicles' path to interact with the Vehicles API");
   },
--- router/vehiclesRouter.js
+++ router/vehiclesRouter.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 
 const vehiclesRouter = new Router();
 
-const { getAllVehicles, getInstructions, redirect } = require('./handlers/vehiclesHandlers');
+const { getAllVehicles, getInstructions, redirect, deleteVehicleById } = require('./handlers/vehiclesHandlers');
 
 vehiclesRouter.get('/', getInstructions);
 
 vehiclesRouter.get('/api/v1/vehicles', getAllVehicles);
+
+vehiclesRouter.delete("/api/v1/vehicles/:id", deleteVehicleById);
 
 vehiclesRouter.get('/*', redirect);
 
